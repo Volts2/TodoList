1106d870a701446589915a38a76918e073bce4d8
Volts <124423370+Volts2@users.noreply.github.com>
2023-10-11 18:43:21 +0700

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="index.css">
+  <title>Daftar</title>
+</head>
+<body>
+    <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form action="">
+                    <h2>Register</h2>
+                    <div class="inputbox">
+                    <ion-icon name="person-outline"></ion-icon>
+                        <input type="Username" required>
+                        <label for="">Username</label>
+                    </div>
+                    <div class="inputbox">
+                    <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                    <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" required>
+                        <label for="">Password</label>
+                    </div>
+                    <button>Register</button>
+                    <div class="register">
+                        <p>have a account <a href="index.php">Login</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
